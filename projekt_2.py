cf5227c0c629712e9caa53af908b9ba49b901994
VratislavCZ <abbadc@gmail.com>
2023-07-22 19:50:38 +0200

    uprava zadani

--- projekt_2.py
+++ projekt_2.py
@@ -27,14 +27,16 @@ plocha ="+---+---+---+\n" \
 
 print("Welcome to Tic Tac Toe")
 print(oddelovac_v)
-print('''GAME RULES:
+print("""GAME RULES:
 Each player can place one mark (or stone)
 per turn on the 3x3 grid. The WINNER is
 who succeeds in placing three of their
-marks in a:
-* horizontal,
-* vertical or
-* diagonal row''')
+marks in a: """)
+red_code = '\033[31m'   
+print(red_code +"* horizontal,")
+print("* vertical or")
+print("* diagonal row")
+print('\033[0m', end='')
 print(oddelovac_v)
 print("Let's start the game")
 print(oddelovac_m)
