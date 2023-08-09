2a73af564b5cc457ef2ed7cdc9ae80a4c7623d38
VratislavCZ <abbadc@gmail.com>
2023-08-09 16:54:46 +0200

    Comments

--- projekt_2.py
+++ projekt_2.py
@@ -21,21 +21,29 @@ def help_board():
 
     return help_board
 
-
+# Print a welcome message
 print("Welcome to Tic Tac Toe")
 print(oddelovac_v)
+
+# Print game rules
 print("""GAME RULES:
 Each player can place one mark (or stone)
 per turn on the 3x3 grid. The WINNER is
 who succeeds in placing three of their
 marks in a: """)
+
+# Set the color code
 red_code = '\033[31m'   
 print(red_code +"* horizontal,")
 print("* vertical or")
 print("* diagonal row")
 print('\033[0m', end='')
+
 print(oddelovac_v)
+
+# Print a message to start the game
 print("Let's start the game")
+
 print(oddelovac_m)
 
 # game board during the game
