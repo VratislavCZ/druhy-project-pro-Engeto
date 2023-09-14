3ccacbc0e3ae6690adaf88453b382bac8d55984e
VratislavCZ <137400733+VratislavCZ@users.noreply.github.com>
2023-09-14 17:03:22 +0200

    Update separator in  play_board.py

--- play_board.py
+++ play_board.py
@@ -1,17 +1,19 @@
 # soubor board.py
 
+separator = "+---+---+---+"
+
 # Helpful game board
 def helping_board():
     for i in range(3):
-        print("+---+---+---+")
+        print(separator)
         for j in range(3):
             print(f"| {i * 3 + j + 1} ", end="")
         print("|")
-    print("+---+---+---+")
+    print(separator)
 
 # Game board during the game
 def printing_board(board):
-    print("+---+---+---+")
+    print(separator)
     for i in range(3):
         print("| {} | {} | {} |".format(*board[i*3:i*3+3]))
-        print("+---+---+---+")
+        print(separator)
